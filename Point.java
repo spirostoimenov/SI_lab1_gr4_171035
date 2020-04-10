49cd23083109eb5b805293470b68bd8a44785671
spirostoimenov <spiro.stoimenov@students.finki.ukim.mk>
2020-04-11 01:53:52 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,22 +1,68 @@
 class Point {
 	String id
 	double x,y;
-        String boja;
 
 	//TODO add new variable
-
+	String boja;
 	//TODO constructor
-
+	public Point(String id, double x, double y, String boja) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
 	//TODO setters and getters
+	public String getId() {
+		return id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public String getBoja() {
+		return boja;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection == 'R'){
+			x++;
+		}else if(xDirection == 'L'){
+			x--;
+		}
+		if(yDirection == 'U'){
+			y++;
+		}
+		else if(yDirection == 'D'){
+			y--;
+		}
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println("Tochkata se naogja na lokacija so koordinati"+ getX() + getY());
 	}
-
-
-
 }
+
+
+
